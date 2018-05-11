a0ca6d712154d1772a1d510dd05889c49522d823
ThakshilaSilva <thakshilasilva.15@cse.mrt.ac.lk>
2018-05-11 19:12:56 +0530

    newq

--- controllers/batchController.js
+++ controllers/batchController.js
@@ -1,29 +1,36 @@
-const { connection } = require('../dbConnection');
+const { getConnection } = require('../dbConnection');
 
 const addEvent = (event) => {
     return new Promise((resolve, reject) => {
-        connection.connect();
-        connection.query("INSERT INTO batchevent VALUE(?,?,?,?,?,?,?,?,?,?,?,?)", [
-            null,
-            event.eventName,
-            event.date,
-            event.description,
-            event.photo1,
-            event.photo2,
-            event.photo3,
-            event.photo4,
-            event.photo5,
-            event.album1,
-            event.album2,
-            event.album3
-        ], (err, res) => {
-            connection.end();
-            if (err) {
-                reject(err);
-            }
-            resolve(res);
-        });
 
+        getConnection(function(err, connection) {
+            if (err) {
+                connection.release();
+                console.log(' Error getting mysql_pool connection: ' + err);
+                throw err;
+            }
+            connection.query("INSERT INTO batchevent VALUE(?,?,?,?,?,?,?,?,?,?,?,?)", [
+                null,
+                event.eventName,
+                event.date,
+                event.description,
+                event.photo1,
+                event.photo2,
+                event.photo3,
+                event.photo4,
+                event.photo5,
+                event.album1,
+                event.album2,
+                event.album3
+            ], (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+
+            connection.release();
+        })
     }).catch((error) => {
         reject(error);
     });
@@ -31,23 +38,29 @@ const addEvent = (event) => {
 
 const addAcaEvent = (event) => {
     return new Promise((resolve, reject) => {
-        connection.end();
 
-        connection.query("INSERT INTO upcomingevent VALUE(?,?,?,?,?)", [
-            null,
-            event.module,
-            event.name,
-            event.description,
-            event.date
-        ], (err, res) => {
-            connection.connect();
+        getConnection(function(err, connection) {
             if (err) {
-                reject(err);
-                console.log(err);
+                connection.release();
+                console.log(' Error getting mysql_pool connection: ' + err);
+                throw err;
             }
-            resolve(res);
-        });
+            connection.query("INSERT INTO upcomingevent VALUE(?,?,?,?,?)", [
+                null,
+                event.module,
+                event.name,
+                event.description,
+                event.date
+            ], (err, res) => {
 
+                if (err) {
+                    reject(err);
+                    console.log(err);
+                }
+                resolve(res);
+            });
+            connection.release();
+        })
     }).catch((error) => {
         reject(error);
     });
@@ -57,17 +70,23 @@ const addAcaEvent = (event) => {
 
 const getEvents = () => {
     return new Promise((resolve, reject) => {
-        connection.end();
 
-        connection.query("SELECT * FROM batchevent",
-            (err, res) => {
-                connection.connect();
-                if (err) {
-                    reject(err);
-                }
-                resolve(res);
-            });
-
+        getConnection(function(err, connection) {
+            if (err) {
+                connection.release();
+                console.log(' Error getting mysql_pool connection: ' + err);
+                throw err;
+            }
+            connection.query("SELECT * FROM batchevent",
+                (err, res) => {
+                    connection.connect();
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(res);
+                });
+            connection.release();
+        })
     }).catch((error) => {
         reject(error);
     });
@@ -75,33 +94,43 @@ const getEvents = () => {
 
 const getAcaEvents = () => { // query every student from the database
     return new Promise((resolve, reject) => {
-        connection.end();
-
-        connection.query("select * from upcomingevent",
-            (err, res) => {
-                connection.connect();
-                if (err) {
-                    reject(err);
-                }
-                resolve(res);
-            });
-
+        getConnection(function(err, connection) {
+            if (err) {
+                connection.release();
+                console.log(' Error getting mysql_pool connection: ' + err);
+                throw err;
+            }
+            connection.query("select * from upcomingevent",
+                (err, res) => {
+                    connection.connect();
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(res);
+                });
+            connection.release();
+        })
     });
 };
 
 const getEditableAcaEvent = (item) => {
     return new Promise((resolve, reject) => {
-
-        connection.connect();
-        connection.query("SELECT * FROM upcomingevent WHERE EventID=?", [item.EventID],
-            (err, res) => {
-                connection.end();
-                if (err) {
-                    reject(err);
-                }
-                resolve(res);
-            });
-
+        getConnection(function(err, connection) {
+            if (err) {
+                connection.release();
+                console.log(' Error getting mysql_pool connection: ' + err);
+                throw err;
+            }
+            connection.query("SELECT * FROM upcomingevent WHERE EventID=?", [item.EventID],
+                (err, res) => {
+                    connection.end();
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(res);
+                });
+            connection.release();
+        })
     }).catch((error) => {
         reject(error);
     });
@@ -109,17 +138,22 @@ const getEditableAcaEvent = (item) => {
 
 const getEditableBatchEvent = (item) => {
     return new Promise((resolve, reject) => {
-
-        connection.connect();
-        connection.query("SELECT * FROM batchEvent WHERE EventID=?", [item.EventID],
-            (err, res) => {
-                connection.end();
-                if (err) {
-                    reject(err);
-                }
-                resolve(res);
-            });
-
+        getConnection(function(err, connection) {
+            if (err) {
+                connection.release();
+                console.log(' Error getting mysql_pool connection: ' + err);
+                throw err;
+            }
+            connection.query("SELECT * FROM batchEvent WHERE EventID=?", [item.EventID],
+                (err, res) => {
+                    connection.end();
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(res);
+                });
+            connection.release();
+        })
     }).catch((error) => {
         reject(error);
     });
@@ -128,22 +162,30 @@ const getEditableBatchEvent = (item) => {
 const updateAcaEvent = (event) => {
 
     return new Promise((resolve, reject) => {
-        connection.connect();
-        connection.query("UPDATE upcomingevent SET ModuleCode=?, ModuleName=?, EventDetail=? WHERE EventID=?", [
-            event.code,
-            event.name,
-            event.detail,
-            event.ID
-        ], (err, result) => {
-            connection.end();
-            if (err) {
-                reject(err);
-                console.log("ERROR " + err);
-            }
-            resolve(result);
-            console.log("res " + result);
-        });
 
+        getConnection(function(err, connection) {
+            if (err) {
+                connection.release();
+                console.log(' Error getting mysql_pool connection: ' + err);
+                throw err;
+            }
+            connection.query("UPDATE upcomingevent SET ModuleCode=?, ModuleName=?, EventDetail=? WHERE EventID=?", [
+                event.code,
+                event.name,
+                event.detail,
+                event.ID
+            ], (err, result) => {
+                connection.end();
+                if (err) {
+                    reject(err);
+                    console.log("ERROR " + err);
+                }
+                resolve(result);
+                console.log("res " + result);
+            });
+
+            connection.release();
+        })
     }).catch((error) => {
         console.log(error);
         reject(error);
@@ -153,24 +195,31 @@ const updateAcaEvent = (event) => {
 const updateBatchEvent = (event) => {
 
     return new Promise((resolve, reject) => {
-        connection.connect();
-        connection.query("UPDATE batchEvent SET EventName=?, EventDescription=?, photoAlbum1=?, photoAlbum2=?, photoAlbum3=? WHERE EventID=?", [
-            event.name,
-            event.description,
-            event.album1,
-            event.album2,
-            event.album3,
-            event.ID
-        ], (err, result) => {
-            connection.end();
-            if (err) {
-                reject(err);
-                console.log("ERROR " + err);
-            }
-            resolve(result);
-            console.log("res " + result);
-        });
 
+        getConnection(function(err, connection) {
+            if (err) {
+                connection.release();
+                console.log(' Error getting mysql_pool connection: ' + err);
+                throw err;
+            }
+            connection.query("UPDATE batchEvent SET EventName=?, EventDescription=?, photoAlbum1=?, photoAlbum2=?, photoAlbum3=? WHERE EventID=?", [
+                event.name,
+                event.description,
+                event.album1,
+                event.album2,
+                event.album3,
+                event.ID
+            ], (err, result) => {
+                connection.end();
+                if (err) {
+                    reject(err);
+                    console.log("ERROR " + err);
+                }
+                resolve(result);
+                console.log("res " + result);
+            });
+            connection.release();
+        })
     }).catch((error) => {
         console.log(error);
         reject(error);
